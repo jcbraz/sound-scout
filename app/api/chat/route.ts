a195a22618b22388c60ac33614ccdd48285caede
jcbraz <josecbraz@outlook.com>
2023-11-14 20:25:28 +0100

    Fix openai key

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,7 +3,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 export const runtime = 'edge'
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY })
 
 export async function POST(req: Request) {
     // Extract the `messages` from the body of the request
