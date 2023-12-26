c1a439dc570f6aa1f6aff5115ef4070f0589e5b3
jcbraz <josecbraz@outlook.com>
2023-12-26 22:50:55 +0000

    fix: remove unecessary arguments from useTypewrite hook in main headers

--- components/ui/headers.tsx
+++ components/ui/headers.tsx
@@ -4,7 +4,7 @@ import { Logo } from "./logo";
 import useTypewriter from "./use-type-writter";
 
 const Headers = () => {
-  const displayText = useTypewriter("t/he spotify playlist engine", 50, true);
+  const displayText = useTypewriter("t/he spotify playlist engine", 50);
 
   return (
     <div className="flex flex-col items-center justify-center space-y-6">
